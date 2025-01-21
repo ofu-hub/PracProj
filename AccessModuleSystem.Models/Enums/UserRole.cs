a18cdca2b21ecfa026d089863777f80d51c5441f
Pavel Bulatov <pmarkelo77@gmail.com>
2025-01-21 15:49:39 +0400

    Edit

--- AccessModuleSystem.Models/Enums/UserRole.cs
+++ AccessModuleSystem.Models/Enums/UserRole.cs
@@ -2,17 +2,32 @@
 
 namespace AccessModuleSystem.Models.Enums;
 
+/// <summary>
+/// Роли пользователей
+/// </summary>
 public enum UserRole
 {
+  /// <summary>
+  /// Администратор
+  /// </summary>
   [Display(Name = "Администратор")]
-  Admin,
+  Admin = 0,
 
+  /// <summary>
+  /// Пользователь
+  /// </summary>
   [Display(Name = "Пользователь")]
-  User,
+  User = 1,
 
+  /// <summary>
+  /// Оператор
+  /// </summary>
   [Display(Name = "Оператор")]
-  Operator,
+  Operator = 2,
 
+  /// <summary>
+  /// Инженер
+  /// </summary>
   [Display(Name = "Инженер")]
-  Engineer
+  Engineer = 3
 }
